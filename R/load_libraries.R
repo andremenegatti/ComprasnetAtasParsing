216bce261a5f554c83ba9d093723a0770d3360a5
André Luís Menegatti <>
2019-04-08 14:16:05 -0300

    Delete load_libraries.R

--- R/load_libraries.R
+++
@@ -1,4 +0,0 @@
-suppressPackageStartupMessages(library(dplyr))
-suppressPackageStartupMessages(library(purrr))
-suppressPackageStartupMessages(library(rvest))
-suppressPackageStartupMessages(library(stringr))
