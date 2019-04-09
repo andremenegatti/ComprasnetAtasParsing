142bb69753b38843c1d35716ab696e555203dc60
André Luís Menegatti <>
2019-04-09 16:49:03 -0300

    Documentação

--- R/get_segundos.R
+++ R/get_segundos.R
@@ -5,7 +5,7 @@
 #' @param date_string  character string com uma data no formato utilizado nas atas ou em formato ISO-8601
 #' @param iso_input boolean indicando se o argumento date_string esta em formato ISO-8601 ou nao
 #'
-#' @return
+#' @return segundos
 #' @export
 get_segundos <- function(date_string, iso_input = FALSE) {
 
