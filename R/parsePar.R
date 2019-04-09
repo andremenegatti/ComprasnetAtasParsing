c09ee1759daeab21ae3917256bcf7a6dda3aa67c
André Luís Menegatti <>
2019-04-09 14:10:17 -0300

    Imports

--- R/parsePar.R
+++ R/parsePar.R
@@ -1,11 +1,11 @@
 parsePar <- function(nworkers) {
 
-  registerDoParallel(cores = nworkers)
+  doParallell::registerDoParallel(cores = nworkers)
 
-  df_dados <- foreach(ata = atas_files,
-                      .combine = bind_rows,
-                      .export = c("atas_files", "parse_ata"),
-                      .packages = c("dplyr", "purrr", "rvest", "stringr", "lubridate", "ComprasnetAtasParsing")) %dopar%
+  df_dados <- foreach::foreach(ata = atas_files,
+                               .combine = bind_rows,
+                               .export = c("atas_files", "parse_ata"),
+                               .packages = c("ComprasnetAtasParsing")) %dopar%
     parse_ata(ata_html_file = ata)
 
   df_dados
